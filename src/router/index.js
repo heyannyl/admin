d323b117d871b0730f7a41c4f2f401f71ec44af2
canbefree <452198757@qq.com>
2018-08-30 09:33:03 +0800

    wangedit

--- src/router/index.js
+++ src/router/index.js
@@ -7,41 +7,50 @@ import UserProfile from '@/components/content/demo1'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/home',
-      component: Main,
-    } ,
-    {
-      path: '/',
-      name: '_home',
-      redirect: '/home',
-      component: Main,
-      // meta: {
-      //   hideInMenu: true,
-      //   notCache: true
-      // },
-      children: [
+    routes: [
         {
-          path: 'demo1',
-          name: 'demo1',
-          meta: {
-            icon: 'md-trending-up',
-            title: '数字渐变'
-          },
-          component: UserProfile,
+            path: '/home',
+            component: Main,
         },
         {
-          path: 'demo2',
-          name: 'demo2',
-          meta: {
-            icon: 'md-trending-up',
-            title: '数字渐变'
-          },
-          component: () => import('@/components/content/demo2')
+            path: '/',
+            name: '_home',
+            redirect: '/home',
+            component: Main,
+            // meta: {
+            //   hideInMenu: true,
+            //   notCache: true
+            // },
+            children: [
+                {
+                    path: 'demo1',
+                    name: 'demo1',
+                    meta: {
+                        icon: 'md-trending-up',
+                        title: '数字渐变'
+                    },
+                    component: UserProfile,
+                },
+                {
+                    path: 'demo2',
+                    name: 'demo2',
+                    meta: {
+                        icon: 'md-trending-up',
+                        title: '数字渐变'
+                    },
+                    component: () => import('@/components/content/demo2')
+                },
+                {
+                    path: 'demo3',
+                    name: 'demo3',
+                    meta: {
+                        icon: 'md-trending-up',
+                        title: '数字渐变'
+                    },
+                    component: () => import('@/components/content/demo3')
+                },
+            ]
         },
-        ]
-    },
 
-  ]
+    ]
 })
